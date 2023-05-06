e6cd78c8d56fbf6ade412c5ec91263b724ab969d
shyjiang233 <shyjiang233@gmail.com>
2023-05-06 21:30:42 +0800

    `Merge remote-tracking branch 'origin/master'`

--- Jvm-Demo-03/src/main/java/common/IOUtils.java
+++ Jvm-Demo-03/src/main/java/common/IOUtils.java
@@ -3,10 +3,8 @@ package common;
 import java.math.BigInteger;
 
 /**
- * http://www.itstack.org
- * create by fuzhengwei on 2019/5/13
- * <p>
- * java虚拟机定义了u1、u2、u4三种数据类型来表示；1字节、2字节、4字节，无符号整数。
+ Author:shyjiang233
+ * Desc:java虚拟机定义了u1、u2、u4三种数据类型来表示；1字节、2字节、4字节，无符号整数。
  * 在如下实现中，用增位方式表示无符号类型：
  * u1、u2可以用int类型存储，因为int类型是4字节
  * u4 需要用long类型存储，因为long类型是8字节
@@ -70,14 +68,14 @@ public class IOUtils {
     public byte[] readBytes(int n) {
         return readByte(n);
     }
-
+    /*从一个字节数组中读取指定长度的字节，并返回一个新的字节数组，其中包含了读取的这些字节*/
     private byte[] readByte(int length) {
-        byte[] copy = new byte[length];
-        System.arraycopy(data, 0, copy, 0, length);
-        System.arraycopy(data, length, data, 0, data.length - length);
+        byte[] copy = new byte[length];// 创建一个与指定长度相等的新字节数组
+        System.arraycopy(data, 0, copy, 0, length);// 将 data 数组中前 length 个字节复制到新数组 copy 中
+        System.arraycopy(data, length, data, 0, data.length - length);// 将 data 数组中第 length 个字节到最后一个字节向前移动 length 个位置
         return copy;
     }
-
+/*将字节数组转换为对应的整数值*/
     private int byte2int(byte[] val) {
         String str_hex = new BigInteger(1, val).toString(16);
         return Integer.parseInt(str_hex, 16);
